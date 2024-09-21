e55ef6c8c51d007f9818f202026e1907725383a1
GE08T <gaerthamgk@gmail.com>
2024-09-21 19:43:09 +0700

    feat(backend): routing url

--- server/core/App.php
+++ server/core/App.php
@@ -3,6 +3,16 @@
 // class ini digunakan untuk berjalannya aplikasi
 class App {
   public function __construct()  {
-    var_dump($_GET);
+    $url = $this->parseUrl();
+    var_dump($url);
+  }
+
+  public function parseUrl() {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url']);
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
