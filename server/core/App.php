28c4687df226a0c5d05e2398a0b0d22f21a1c4b4
GE08T <gaerthamgk@gmail.com>
2024-09-21 20:22:22 +0700

    feat(backend) : url compatible with controller, method, and params

--- server/core/App.php
+++ server/core/App.php
@@ -2,9 +2,42 @@
 
 // class ini digunakan untuk berjalannya aplikasi
 class App {
+  protected $controller = "Home";
+  protected $method = "index";
+  protected $params = [];
+
   public function __construct()  {
     $url = $this->parseUrl();
-    var_dump($url);
+
+    // check jika mengirim url kosong
+    if (is_null($url)) {
+      $url[0] = $this->controller;
+    }
+
+    // chek apakah ada file controller di folder
+    // jika ada maka diganti ke controller yang ada
+    if(file_exists('./server/controller/' . $url[0] . '.php')) {
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+    require_once './server/controller/' . $this->controller . '.php';
+    $this->controller = new $this->controller;
+
+    // check method nya apa ada jika ada maka diganti
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    // check params jika ada maka di simpan dalam $params
+    if (!empty($url)) {
+      $this->params = array_values($url);
+    }
+
+    // menjalankan controller & method, serta kirimkan params jika ada
+    call_user_func_array([$this->controller, $this->method], $this->params);
   }
 
   public function parseUrl() {
